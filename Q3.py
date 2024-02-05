024faf21a9cce34ed541f0858a0fb118792a4523
Rida Shaik <101663159+RidaShaik@users.noreply.github.com>
2024-02-05 16:07:20 -0500

    Update Q3.py
    Fixed bisectionMethod, getting complex number error for function three

--- Q3.py
+++ Q3.py
@@ -12,7 +12,7 @@ def rootSolver(f, a, b, Po, TOL):
         steps += 1
 
     if c.real >= a and c.real <= b:
-        return c.real, steps
+        return c.real
     else:
         return bisectionMethod(f, a, b, Po, TOL)
 
@@ -20,8 +20,20 @@ def bisectionMethod(f, a, b, Po, TOL):
     low = a
     high = b
 
-    steps = 0
-    maxIter = 1000
+    print (low, " ", high, " ", f(low), " ", f(high))
+    if f(low) * f(high) >= 0:
+        return
+    while high - low >= TOL:
+        c = (low + high) / 2
+        if (f(c) == 0.0):
+            break
+        if f(c) * f(low) < 0:
+            high = c
+        else:
+            low = c
+    return c
+
+'''
     while steps < maxIter:
         m = (low + high) / 2.0
         if m == 0 or abs(f(high - low)) < TOL:
@@ -32,12 +44,13 @@ def bisectionMethod(f, a, b, Po, TOL):
 
     estimate = (low + high) / 2.0
     return estimate, steps
-
+'''
 
 #f = lambda x: x**3 - 2
 #f = lambda x: (x-2)**2
 f = lambda x: x ** (1/3)
-val, steps = rootSolver(f, -1, 1, .5, 0.01)
-print ("Root: ", val)
-print ("Steps taken:", steps)
 
+print(f(-1))
+
+#val = rootSolver(f, -1, 1, 0.5, 0.01)
+#print ("Root: ", val)
