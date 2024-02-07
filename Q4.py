e406bc1034cb362dd5b6e786ffb7ccdc576f98b4
Rida Shaik <101663159+RidaShaik@users.noreply.github.com>
2024-02-07 16:37:49 -0500

    Final update, fixed plot and zero issues

--- Q4.py
+++ Q4.py
@@ -10,17 +10,28 @@ def ieee_baby(num):
         sign = 1
     else:
         sign = -1
-    med = int(two, 2)
-    med = 2**(med - 1)
-    frac = int(three, 2)
-    frac = 2 ** (-1.0 * frac)
-    frac = 1.0 + frac
 
-    return sign * med * frac
+    frac = 0
+    if three[0] == '1':
+        frac = frac + (2 ** -1)
+    if three[1] == '1':
+        frac = frac + (2 ** -2)
+    if three[2] == '1':
+        frac = frac + (2 ** -3)
+
+    med = int(two, 2)
+    if med == 0:
+        med = 2 ** -1
+        return sign * med * frac
+    else:
+        med = 2 ** (med - 1)
+        frac = 1.0 + frac
+        return sign * med * frac
+
 
 
 #Calling ieee_baby function
-print("IEEE baby decimal value of 010010 is: ", ieee_baby('010010'))
+print("IEEE baby decimal value of 001000 is: ", ieee_baby('001000'))
 
 
 #Part A
@@ -32,21 +43,25 @@ for a in range(2):
                 for e in range(2):
                     for f in range(2):
                         value = f'{a}{b}{c}{d}{e}{f}'
-                        print(value, " ", ieee_baby(value))
                         babyList.append(ieee_baby(value))
 
 totalNum = 0
-for element in babyList:
+babySet = set(babyList)
+for element in babySet:
     totalNum = totalNum + 1
 print("Total number of unique values: ", totalNum)
-plt.plot(babyList)
+plt.scatter(list(babySet), [0] * len(babySet))
+plt.yticks([])
+plt.grid(True)
 plt.show()
+
 
 #Part B
 #Finding Largest Gap
 i = 1
-while i < (len(babyList)):
-    x = abs(babyList[i] - babyList[i - 1])
+sortedList = sorted(babyList)
+while i < (len(sortedList)):
+    x = abs(sortedList[i] - sortedList[i - 1])
     if i == 1:
         bigGap = x
     else:
@@ -56,13 +71,14 @@ while i < (len(babyList)):
 print("Largest gap size: ", bigGap)
 #Finding Smallest Gap
 i = 1
-while i < (len(babyList)):
-    x = abs(babyList[i] - babyList[i - 1])
-    if i == 1:
-        smallGap = x
-    else:
-        if x <= smallGap:
+while i < (len(sortedList)):
+    x = abs(sortedList[i] - sortedList[i - 1])
+    if x != 0:
+        if i == 1:
             smallGap = x
+        else:
+            if x <= smallGap:
+                smallGap = x
     i = i + 1
 print("Smallest gap size: ", smallGap)
 #Smallest Positive Number
